87f38fd205a42cea5261173b7439f9b50b029ba3
kareraolivier <kareraolivier@gmail.com>
2024-01-21 18:06:55 +0200

    feat: setup is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-function App() {
-  return (
-    <p className="text-3xl font-bold text-green-600">
-      Click on the Vite and React logos to learn more
-    </p>
-  );
+import { Component } from "react";
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="m-4 text-lg">{this.state.count}</h1>
+        <button
+          onClick={() => this.setState({ count: this.state.count + 1 })}
+          className="bg-green-600 text-white p-2 rounded-sm shadow-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default MyComponent;
